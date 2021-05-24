04f168a43a1a47a88c925e331d62804193c73f99
Leonardo Meira <meiraleoabr@gmail.com>
2021-05-24 10:24:49 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const connection = new Sequelize('guiaperguntas', 'root', '562347', {
+const connection = new Sequelize('guiaperguntas', 'root', 'insirasenha', {
     host: 'localhost',
     dialect: 'mysql'
 })
